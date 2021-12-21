5f193967ed1243baaea94f31a5d70da6115f0e3e
kimsojin <thwls0807@naver.com>
2021-12-21 17:33:29 +0900

    modifiy project3

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DemoApplication {
-//ㅎㅎ
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
     }
-
 }
